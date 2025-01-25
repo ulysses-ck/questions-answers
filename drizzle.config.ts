f7919a9d92eee88379b0125489c89cd0d286f8b1
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-25 17:22:08 -0300

    chore: update Drizzle configuration with output directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: 'postgresql', // 'mysql' | 'sqlite' | 'turso'
-  schema: './src/db/schema.ts',
+  out: "./drizzle",
+  dialect: "postgresql", // 'mysql' | 'sqlite' | 'turso'
+  schema: "./src/db/schema.ts",
   dbCredentials: {
-    url: process.env.DATABASE_URL!
-  }
-})
+    url: process.env.DATABASE_URL!,
+  },
+});
