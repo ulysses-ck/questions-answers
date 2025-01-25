099ab00d5bb2fde6bdeb0381c86cf3a03b98a368
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-25 02:27:00 -0300

    chore: integrate HeroUI components and theme with Next.js project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/theme";
 
 export default {
   content: [
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +16,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 } satisfies Config;
